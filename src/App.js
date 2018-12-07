70f6b04ec8aceb4d463706968dc75de963ba4cec
UK\Billy.Bellchambers <Billy.bellchambers@theaccessgroup.com>
2018-12-07 14:39:20 +0000

    Basic Router set-up

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <Route exact path="/" render={() => <h1>Home</h1>} />
+      <Route  path="/about" render={() => <h1>About</h1>} />
+      <Route  path="/contact" render={() => <h1>Contact</h1>} />    
+    </div>
+  </Router>
+);
 
 export default App;
