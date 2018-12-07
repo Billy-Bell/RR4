14be4de89f1e5953fe7499f1bcb3ff5373c964b3
UK\Billy.Bellchambers <Billy.bellchambers@theaccessgroup.com>
2018-12-07 16:40:40 +0000

    Additional routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {
+  BrowserRouter as Router, 
+  Route, 
+  NavLink
+} from 'react-router-dom';
+
+import './App.css';
+
+const isActiveFunc = (match, location) => {
+  console.log(match,location)
+  return match
+}
+
+const Links = () => (
+  <nav>
+    <NavLink exact activeClassName="active" to="/">Home</NavLink>
+    <NavLink activeStyle={{color:'green'}} to='/about'>About</NavLink>
+    <NavLink 
+      isActive={isActiveFunc}
+      activeClassName="active" 
+      to='/contact'>Contact</NavLink>
+  </nav>
+)
 
 const App = () => (
   <Router>
     <div>
+      <Links />
       <Route exact path="/" render={() => <h1>Home</h1>} />
       <Route  path="/about" render={() => <h1>About</h1>} />
-      <Route  path="/contact" render={() => <h1>Contact</h1>} />    
+      <Route  path="/contact" render={() => <h1>Contact</h1>} />   
+      <Route  path="/page" render={() => (
+        <h1>
+          PAGE
+        </h1>
+      )} />  
     </div>
   </Router>
 );
